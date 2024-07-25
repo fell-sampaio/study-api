807fbd5c748181b2d94ec74caccb9cd401dd4b4d
Felipe Sampaio <62025480+fell-sampaio@users.noreply.github.com>
2024-07-24 22:18:51 -0300

    DTOs created and removed nullable from API project

---
+++ src/StudyApi.Api/DTOs/AdressDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyApi.Api.DTOs;
+
+public class AdressDto
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(200, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 2)]
+    public string PublicArea { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(50, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 1)]
+    public string Number { get; set; }
+
+    public string AdditionalAdressDetails { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(8, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 8)]
+    public string ZipCode { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(100, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 2)]
+    public string District { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(100, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 2)]
+    public string City { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(50, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 2)]
+    public string State { get; set; }
+
+    public Guid SupplierId { get; set; }
+}
---
+++ src/StudyApi.Api/DTOs/SupplierDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyApi.Api.DTOs;
+
+public class SupplierDto
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(100, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 2)]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "{0} field must be provided")]
+    [StringLength(14, ErrorMessage = "Field {0} must have between {2} and {1} characters", MinimumLength = 11)]
+    public string Document { get; set; }
+
+    public int SupplierType { get; set; }
+    public AdressDto Adress { get; set; }
+    public bool Active { get; set; }
+    public IEnumerable<ProductDto> Products { get; set; }
+}
