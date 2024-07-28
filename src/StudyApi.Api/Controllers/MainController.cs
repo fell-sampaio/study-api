22a5c055b8d26f82a8efb7e321815b72b543f437
Felipe Sampaio <62025480+fell-sampaio@users.noreply.github.com>
2024-07-28 16:44:32 -0300

    Main controller created

---
+++ src/StudyApi.Api/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyApi.Api.Controllers;
+[ApiController]
+public abstract class MainController : ControllerBase
+{
+    // errors notifications validation
+
+    // modelstate validation
+
+    // business operation validation
+}
