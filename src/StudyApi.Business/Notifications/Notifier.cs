fb0c9c33519fc661a10dd02f47bc408ca5269f1a
Felipe Sampaio <62025480+fell-sampaio@users.noreply.github.com>
2024-07-28 16:32:29 -0300

    Notifier class configured to handle notifications

--- src/StudyApi.Business/Notifications/Notifier.cs
+++ src/StudyApi.Business/Notifications/Notifier.cs
@@ -1,4 +1,22 @@
-﻿namespace StudyApi.Business.Notifications;
-public class Notifier
+﻿using StudyApi.Business.Interfaces;
+
+namespace StudyApi.Business.Notifications;
+public class Notifier : INotifier
 {
+    private readonly List<Notification> _notifications = [];
+
+    public void Handle(Notification notification)
+    {
+        _notifications.Add(notification);
+    }
+
+    public List<Notification> GetNotifications()
+    {
+        return _notifications;
+    }
+
+    public bool HasNotification()
+    {
+        return _notifications.Count != 0;
+    }
 }
