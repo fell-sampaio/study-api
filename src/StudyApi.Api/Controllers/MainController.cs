4bae51d4d87c70216fb83456c1fa385b036a84ac
Felipe Sampaio <62025480+fell-sampaio@users.noreply.github.com>
2024-08-18 18:44:58 -0300

    Changing MainController to deal with CustomErrors and notify it

--- src/StudyApi.Api/Controllers/MainController.cs
+++ src/StudyApi.Api/Controllers/MainController.cs
@@ -1,12 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using StudyApi.Business.Interfaces;
+using StudyApi.Business.Notifications;
 
 namespace StudyApi.Api.Controllers;
 [ApiController]
-public abstract class MainController : ControllerBase
+public abstract class MainController(INotifier notifier) : ControllerBase
 {
-    // errors notifications validation
+    private readonly INotifier _notifier = notifier;
 
-    // modelstate validation
+    protected bool ValidOperation()
+    {
+        return !_notifier.HasNotification();
+    }
 
-    // business operation validation
+    protected ActionResult CustomResponse(object result = null)
+    {
+        if (ValidOperation())
+        {
+            return Ok(new
+            {
+                success = true,
+                data = result
+            });
+        }
+
+        return BadRequest(new
+        {
+            susccess = false,
+            errors = _notifier.GetNotifications().Select(n => n.Message)
+        });
+    }
+
+    protected ActionResult CustomResponse(ModelStateDictionary modelState)
+    {
+        if (!modelState.IsValid) NotificateInvalidModelError(modelState);
+
+        return CustomResponse();
+    }
+
+    protected void NotificateInvalidModelError(ModelStateDictionary modelState)
+    {
+        var errors = modelState.Values.SelectMany(e => e.Errors);
+        foreach (var error in errors)
+        {
+            var errorMessage = error.Exception == null ? error.ErrorMessage : error.Exception.Message;
+            NotificateError(errorMessage);
+        }
+    }
+
+    protected void NotificateError(string message)
+    {
+        _notifier.Handle(new Notification(message));
+    }
 }
