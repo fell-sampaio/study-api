ba891423e72ed10b0d26aa7a330d7ac782c61b07
Felipe Sampaio <62025480+fell-sampaio@users.noreply.github.com>
2024-09-01 11:48:02 -0300

    Added endpoints to product controller and a method to handle the image upload (saving in a local folder for now)

---
+++ src/StudyApi.Api/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudyApi.Api.DTOs;
+using StudyApi.Business.Interfaces;
+using StudyApi.Business.Models;
+
+namespace StudyApi.Api.Controllers;
+
+[Route("api/products")]
+public class ProductsController(INotifier notifier, IProductRepository productRepository, IProductService productService, IMapper mapper) : MainController(notifier)
+{
+    [HttpGet]
+    public async Task<IEnumerable<ProductDto>> GetAll()
+    {
+        return mapper.Map<IEnumerable<ProductDto>>(await productRepository.GetSuppliersProducts());
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ProductDto>> GetById(Guid id)
+    {
+        var productDto = await GetProduct(id);
+
+        if (productDto == null) return NotFound();
+
+        return productDto;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProductDto>> Add(ProductDto productDto)
+    {
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+        var imageName = Guid.NewGuid() + "_" + productDto.Image;
+        if (!FileUpload(productDto.ImageUpload, imageName)) return CustomResponse(productDto);
+
+        productDto.Image = imageName;
+        await productService.Add(mapper.Map<Product>(productDto));
+
+        return CustomResponse(productDto);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult<ProductDto>> Delete(Guid id)
+    {
+        var product = await GetProduct(id);
+
+        if (product == null) return NotFound();
+
+        await productService.Delete(id);
+
+        return CustomResponse(product);
+    }
+
+    private bool FileUpload(string file, string imgName)
+    {
+        var imageDataByteArray = Convert.FromBase64String(file);
+
+        if (file == null || file.Length <= 0)
+        {
+            NotificateError("Provide an image for this product!");
+            return false;
+        }
+
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", imgName);
+
+        if (System.IO.File.Exists(filePath))
+        {
+            NotificateError("There is already a file with this name!");
+            return false;
+        }
+
+        System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
+
+        return true;
+    }
+
+    private async Task<ProductDto> GetProduct(Guid id)
+    {
+        return mapper.Map<ProductDto>(await productRepository.GetSupplierProduct(id));
+    }
+}
