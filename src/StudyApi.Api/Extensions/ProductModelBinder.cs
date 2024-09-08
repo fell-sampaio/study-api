b880420243edccc5da16a92db63613bfc2086087
Felipe Sampaio <62025480+fell-sampaio@users.noreply.github.com>
2024-09-08 17:21:20 -0300

    New binder to correctly deal with the image file and throw success or fail depending on the binding

---
+++ src/StudyApi.Api/Extensions/ProductModelBinder.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using StudyApi.Api.DTOs;
+using System.Text.Json;
+
+namespace StudyApi.Api.Extensions;
+
+public class ProductModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        ArgumentNullException.ThrowIfNull(bindingContext);
+
+        JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            WriteIndented = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            PropertyNameCaseInsensitive = true
+        };
+        JsonSerializerOptions serializeOptions = jsonSerializerOptions;
+
+        var productValueProviderResult = bindingContext.ValueProvider.GetValue("product");
+        if (productValueProviderResult != ValueProviderResult.None)
+        {
+            var productString = productValueProviderResult.FirstValue;
+            var produtoImagemViewModel = JsonSerializer.Deserialize<ProductImageDto>(productString, serializeOptions);
+
+            if (bindingContext.ActionContext.HttpContext.Request.Form.Files.Count > 0)
+            {
+                produtoImagemViewModel.ImageUpload = bindingContext.ActionContext.HttpContext.Request.Form.Files[0];
+            }
+
+            bindingContext.Result = ModelBindingResult.Success(produtoImagemViewModel);
+        }
+        else
+        {
+            bindingContext.Result = ModelBindingResult.Failed();
+        }
+
+        return Task.CompletedTask;
+    }
+}
